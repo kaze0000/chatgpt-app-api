06b81ae8a9ce04fccca8217ad4b06fcd6df16b98
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-05-11 23:00:23 +0900

    test

--- pkg/adapters/middleware/cors.go
+++ pkg/adapters/middleware/cors.go
@@ -1,13 +1,16 @@
 package middleware
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func CORSMiddleware(FE_URL string) echo.MiddlewareFunc {
+	fmt.Println("FE_URL", FE_URL)
 	corsMiddleware := middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{FE_URL},
+		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
 	    echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken, echo.HeaderAuthorization},
 		AllowMethods: []string{"GET", "PUT", "POST", "DELETE"},
